03265c5ed6242c13af685b74bb78cc9469e755f7
pshah9 <pshah9@bohg-ws-1.stanford.edu>
2018-01-28 17:58:17 -0800

    fixing png2ppm

--- png2ppm.py
+++ png2ppm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from glob import glob                                                           
 import cv2 
-pngs = glob('./*/*/*.png')
+pngs = glob('/home/linshaonju/BlensorResult_test/*/*.png')
 
 for j in pngs:
     img = cv2.imread(j)
